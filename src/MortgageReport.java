930d6a99b4bd4f02db69bf02a29a10d9058703a6
Emilio Depena <emiliosdep@gmail.com>
2020-11-22 23:09:19 -0500

    mortgage report

---
+++ src/MortgageReport.java
@@ -0,0 +1,31 @@
+import Mortgage.Mortgage;
+
+import java.text.NumberFormat;
+
+public class MortgageReport {
+
+    private final NumberFormat currency;
+    private Mortgage calculator;
+
+    public MortgageReport(Mortgage calculator) {
+        this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
+    }
+
+    public void printPaymentSchedule() {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("------------------");
+        for (double balance : calculator.getRemainingBalances())
+            System.out.println(currency.format(balance));
+    }
+
+    public void printMortgage() {
+        double mortgage = calculator.calculateMortgage();
+        String mortgageFormatted = currency.format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Mortgage: " + mortgageFormatted);
+    }
+}
