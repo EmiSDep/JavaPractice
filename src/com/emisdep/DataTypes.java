d5199d8c162d11e4d2955f4f9ee11d0c5d47aea4
Emilio Depena <emiliosdep@gmail.com>
2020-12-25 21:44:27 -0500

    data types

---
+++ src/com/emisdep/DataTypes.java
@@ -0,0 +1,40 @@
+package com.emisdep;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+        int a;
+        double b;
+        String c;
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        a = scan.nextInt();
+        b = scan.nextDouble();
+        scan.nextLine();
+        c = scan.nextLine();
+        System.out.println(i + a);
+        System.out.println(d + b);
+        System.out.println(s + c);
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+
+        /* Concatenate and print the String variables on a new line;
+        	the 's' variable above should be printed first. */
+
+        scan.close();
+    };
+}
